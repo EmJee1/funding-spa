f47c10bba6cf6304f12cc7e0f0725b47854095cb
EmJee <mart-janroeleveld@outlook.com>
2022-06-11 13:30:17 +0200

    Add mollie configuration

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,29 @@
-import * as functions from "firebase-functions";
+import functions from "firebase-functions"
+import admin from "firebase-admin"
+import express from "express"
+import cors from "cors"
+import mollieClient from "./mollie"
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase)
+
+const app = express()
+
+app.use(cors({ origin: true }))
+
+app.post("/create", async (req, res) => {
+  const { amount, forParticipant } = req.body
+
+  await mollieClient.payments.create({
+    amount: {
+      value: amount,
+      currency: "EUR",
+    },
+    description: "Donation JV sponsor run 2022",
+    metadata: {
+      forParticipant,
+    },
+    redirectUrl: "https://jv-sponsorloop.web.app/done",
+  })
+})
+
+exports.app = functions.https.onRequest(app)
