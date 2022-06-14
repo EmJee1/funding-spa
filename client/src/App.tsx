ddc9b7c9c97b1b6f0f15a23db21936171abb25b1
EmJee <mart-janroeleveld@outlook.com>
2022-06-14 21:03:38 +0200

    Add image with text

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import Container from "react-bootstrap/Container"
 import Donate from "./components/Donate"
 import DonationCount from "./components/DonationCount"
 import BackgroundImage from "./components/BackgroundImage"
+import wellImage from "./assets/well.webp"
 
 const App = () => {
   return (
@@ -21,16 +22,33 @@ const App = () => {
             <DonationCount />
           </Col>
         </Row>
-        <Row>
+        <Row className="mb-5">
           <Col>
             <Donate />
           </Col>
         </Row>
-        <Row>
-          <Col md={6}>
-            <Image />
+        <Row className="info-block">
+          <Col className="info-block-image-container" md={6}>
+            <Image
+              className="info-block-image"
+              src={wellImage}
+              alt="kind aan een waterpomp"
+            />
           </Col>
-          <Col md={6}></Col>
+          <Col className="info-block-text" md={6}>
+            <h4>Lorem ipsum dolor sit amet.</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
+              architecto beatae, ea quasi quibusdam temporibus ut vel
+              voluptatibus? Ipsam porro quidem quisquam vero? Aspernatur
+              cupiditate doloremque quidem reprehenderit sequi! Amet architecto
+              beatae deserunt, doloribus ducimus enim facere id nesciunt officia
+              officiis quaerat quibusdam quod quos, suscipit temporibus totam
+              ullam, vitae. A ad animi hic in inventore modi mollitia nemo non,
+              reprehenderit sapiente suscipit ut veniam, voluptate voluptatem
+              voluptates.
+            </p>
+          </Col>
         </Row>
       </Container>
     </>
